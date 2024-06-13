87385c898025aa71e22de8a0f4935733ab682d25
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-06-13 15:35:58 -0400

    first commit

---
+++ simple-react-application/src/App.jsx
@@ -0,0 +1,35 @@
+import {useState, useEffect} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  // Variable with your API Key
+  const apiKey = "98e3fb1f";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async (searchTerm) => {
+    // Make fetch request and store the response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a JavaScript object
+    const data = await response.json();
+    // Set the Movie state to the received data
+    setMovie(data);
+  };
+
+  // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
